0aad452a8606228191ed493b0ab2c4cfd8c6bf98
arthur <arthur.rios007@gmail.com>
2024-07-15 20:14:25 -0300

    Estrutura da aplicação: Personalizando a StatusBar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable camelcase */
-import { Text, View } from 'react-native'
+import { StatusBar, Text, View } from 'react-native'
 import {
   useFonts,
   Roboto_700Bold,
@@ -10,7 +10,20 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#202024',
+      }}
+    >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
       {fontsLoaded ? <Text>Home</Text> : <View />}
     </View>
   )
