5ae20cd19df2523f7b756656fe34f89d58760b8d
arthur <arthur.rios007@gmail.com>
2024-07-15 20:25:48 -0300

    Component Library: Instalando o gluestack

--- App.tsx
+++ App.tsx
@@ -1,30 +1,33 @@
 /* eslint-disable camelcase */
-import { StatusBar, Text, View } from 'react-native'
+import { StatusBar, View } from 'react-native'
 import {
   useFonts,
   Roboto_700Bold,
   Roboto_400Regular,
 } from '@expo-google-fonts/roboto'
+import { GluestackUIProvider, Text } from '@gluestack-ui/themed'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#202024',
-      }}
-    >
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
+    <GluestackUIProvider>
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: '#202024',
+        }}
+      >
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
 
-      {fontsLoaded ? <Text>Home</Text> : <View />}
-    </View>
+        {fontsLoaded ? <Text>Home</Text> : <View />}
+      </View>
+    </GluestackUIProvider>
   )
 }
