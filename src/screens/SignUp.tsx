603d0cb24e348747f8873811aaa00ec2fa86f664
arthur <arthur.rios007@gmail.com>
2024-07-16 19:19:53 -0300

    Rotas Públicas: Aplicando navegação de autenticação

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -7,12 +7,21 @@ import {
   VStack,
 } from '@gluestack-ui/themed'
 
+import { useNavigation } from '@react-navigation/native'
+
 import BackgroundImg from '@assets/background.png'
 import Logo from '@assets/logo.svg'
+
 import { Input } from '@components/Input'
 import { Button } from '@components/Button'
 
 export function SignUp() {
+  const navigation = useNavigation()
+
+  function handleGoBack() {
+    navigation.goBack()
+  }
+
   return (
     <ScrollView
       contentContainerStyle={{ flexGrow: 1 }}
@@ -52,7 +61,12 @@ export function SignUp() {
             <Button title="Criar e acessar" />
           </Center>
 
-          <Button title="Voltar para o login" variant="outline" mt="$12" />
+          <Button
+            title="Voltar para o login"
+            variant="outline"
+            mt="$12"
+            onPress={handleGoBack}
+          />
         </VStack>
       </VStack>
     </ScrollView>
