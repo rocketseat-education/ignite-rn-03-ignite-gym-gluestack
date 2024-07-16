61b999cf115bfd09e8bafb839f45df5d17178f83
arthur <arthur.rios007@gmail.com>
2024-07-15 21:28:24 -0300

    Component Library: Componente de Input

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,7 +1,8 @@
-import { Center, Image, Text, VStack } from '@gluestack-ui/themed'
+import { Center, Heading, Image, Text, VStack } from '@gluestack-ui/themed'
 
 import BackgroundImg from '@assets/background.png'
 import Logo from '@assets/logo.svg'
+import { Input } from '@components/Input'
 
 export function SignIn() {
   return (
@@ -15,13 +16,22 @@ export function SignIn() {
         position="absolute"
       />
 
-      <Center my="$24">
-        <Logo />
+      <VStack flex={1} px="$10" pb="$16">
+        <Center my="$24">
+          <Logo />
 
-        <Text color="$gray100" fontSize="$sm">
-          Treine sua mente e seu corpo
-        </Text>
-      </Center>
+          <Text color="$gray100" fontSize="$sm">
+            Treine sua mente e seu corpo
+          </Text>
+        </Center>
+
+        <Center gap="$2">
+          <Heading color="$gray100">Acesse sua conta</Heading>
+
+          <Input placeholder="E-mail" />
+          <Input placeholder="Senha" />
+        </Center>
+      </VStack>
     </VStack>
   )
 }
