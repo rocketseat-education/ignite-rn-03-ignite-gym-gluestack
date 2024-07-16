874abe063c5fe1a47d92aff94e61a106d8b04a58
arthur <arthur.rios007@gmail.com>
2024-07-16 19:07:01 -0300

    Rotas Públicas: Criando o contexto de navegação

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import {
 import { GluestackUIProvider } from '@gluestack-ui/themed'
 import { config } from './config/gluestack-ui.config'
 import { Loading } from '@components/Loading'
-import { SignUp } from '@screens/SignUp'
+import { Routes } from '@routes/index'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
@@ -21,7 +21,7 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? <SignUp /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </GluestackUIProvider>
   )
 }
