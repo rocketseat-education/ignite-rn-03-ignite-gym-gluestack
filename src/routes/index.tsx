a4f3f4ff3883d65727b46956231712aa736b2ea1
arthur <arthur.rios007@gmail.com>
2024-07-16 19:38:37 -0300

    Rotas Privadas: Utilizando o Bottom Tab

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,10 @@
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
+import { Box } from '@gluestack-ui/themed'
 
 import { AuthRoutes } from './auth.routes'
+import { AppRoutes } from './app.routes'
+
 import { gluestackUIConfig } from '../../config/gluestack-ui.config'
-import { Box } from '@gluestack-ui/themed'
 
 export function Routes() {
   const theme = DefaultTheme
@@ -11,7 +13,7 @@ export function Routes() {
   return (
     <Box flex={1} bg="$gray700">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        <AppRoutes />
       </NavigationContainer>
     </Box>
   )
