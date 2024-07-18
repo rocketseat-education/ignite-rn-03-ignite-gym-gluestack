8a2f27c2cecde8c4ce7be90a2b33e47f377cf2af
arthur <arthur.rios007@gmail.com>
2024-07-18 20:02:18 -0300

    Finalizando as interfaces: Posicionando na vertical e horizontal

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,16 @@
+import { Heading, HStack, Text, VStack } from '@gluestack-ui/themed'
+
+export function HomeHeader() {
+  return (
+    <HStack>
+      <VStack>
+        <Text color="$gray100" fontSize="$sm">
+          Olá
+        </Text>
+        <Heading color="$gray100" fontSize="$md">
+          Arthur Rios
+        </Heading>
+      </VStack>
+    </HStack>
+  )
+}
