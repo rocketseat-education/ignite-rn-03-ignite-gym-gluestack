a6d765ede6548d80173726868341dca6fdf9101c
arthur <arthur.rios007@gmail.com>
2024-07-18 20:15:53 -0300

    Finalizando as interfaces: Botão Logout

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,12 +1,13 @@
-import { Heading, HStack, Text, VStack } from '@gluestack-ui/themed'
+import { Heading, HStack, Icon, Text, VStack } from '@gluestack-ui/themed'
 import { UserPhoto } from './UserPhoto'
+import { LogOut } from 'lucide-react-native'
 
 export function HomeHeader() {
   return (
     <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap='$4'>
       <UserPhoto source={{ uri: 'https://github.com/arthurrios.png'}} w='$16' h='$16' alt='Imagem do usuário' />
 
-      <VStack>
+      <VStack flex={1}>
         <Text color="$gray100" fontSize="$sm">
           Olá
         </Text>
@@ -14,6 +15,9 @@ export function HomeHeader() {
           Arthur Rios
         </Heading>
       </VStack>
+
+      <Icon as={LogOut} color='$gray200' size='xl' />
+
     </HStack>
   )
 }
