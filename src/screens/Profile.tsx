553f5d95c6b79aa3c681eb95b4d1a0505883450d
arthur <arthur.rios007@gmail.com>
2024-07-19 20:21:41 -0300

    Finalizando as interfaces: Header do Perfil do Usuário

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,10 @@
-import { Center, Text } from '@gluestack-ui/themed'
+import { ScreenHeader } from '@components/ScreenHeader'
+import { VStack } from '@gluestack-ui/themed'
 
 export function Profile() {
   return (
-    <Center flex={1}>
-      <Text color="$white">Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+    </VStack>
   )
 }
