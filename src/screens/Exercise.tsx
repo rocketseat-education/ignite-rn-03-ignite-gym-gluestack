24cd367e9f0f46ad2b7c0b48107123abc9c71f65
arthur <arthur.rios007@gmail.com>
2024-07-19 20:54:48 -0300

    Finalizando as interfaces: Criando Botão de Voltar

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,9 +1,22 @@
-import { Center, Text } from '@gluestack-ui/themed'
+import { Icon, VStack } from '@gluestack-ui/themed'
+import { useNavigation } from '@react-navigation/native'
+import { AppNavigatorRoutesProps } from '@routes/app.routes'
+import { ArrowLeft } from 'lucide-react-native'
+import { TouchableOpacity } from 'react-native'
 
 export function Exercise() {
+  const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+  function handleGoBack() {
+    navigation.goBack()
+  }
   return (
-    <Center flex={1}>
-      <Text color="$white">Exercise</Text>
-    </Center>
+    <VStack flex={1}>
+      <VStack px="$8" bg="$gray600" pt="$12">
+        <TouchableOpacity onPress={handleGoBack}>
+          <Icon as={ArrowLeft} color="$green500" size="xl" />
+        </TouchableOpacity>
+      </VStack>
+    </VStack>
   )
 }
