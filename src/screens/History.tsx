65122a75b54cece431353c517887cbf19d71b894
arthur <arthur.rios007@gmail.com>
2024-07-19 20:18:28 -0300

    Finalizando as interfaces: Utilizando SectionList

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,12 +1,46 @@
 import { HistoryCard } from '@components/HistoryCard'
 import { ScreenHeader } from '@components/ScreenHeader'
-import { VStack } from '@gluestack-ui/themed'
+import { Heading, Text, VStack } from '@gluestack-ui/themed'
+import { useState } from 'react'
+import { SectionList } from 'react-native'
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: '22.07.24',
+      data: ['Puxada frontal', 'Remada unilateral'],
+    },
+    {
+      title: '23.07.24',
+      data: ['Puxada frontal'],
+    },
+  ])
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico" />
-      <HistoryCard />
+
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <HistoryCard />}
+        renderSectionHeader={({ section }) => (
+          <Heading color="$gray200" fontSize="$md" mt="$10" mb="$3">
+            {section.title}
+          </Heading>
+        )}
+        style={{ paddingHorizontal: 32 }}
+        contentContainerStyle={
+          exercises.length === 0 && { flex: 1, justifyContent: 'center' }
+        }
+        ListEmptyComponent={() => (
+          <Text color="$gray200" textAlign="center">
+            Não há exercícios registrados ainda. {'\n'}
+            Vamos fazer execícios hoje?
+          </Text>
+        )}
+        showsVerticalScrollIndicator={false}
+      />
     </VStack>
   )
 }
