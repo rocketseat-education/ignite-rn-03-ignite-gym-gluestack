de8bee20fb8a7ec70f47a977a4b6be46d6c27128
arthur <arthur.rios007@gmail.com>
2024-07-19 20:24:35 -0300

    Finalizando as interfaces: Foto do usuário

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,10 +1,22 @@
 import { ScreenHeader } from '@components/ScreenHeader'
-import { VStack } from '@gluestack-ui/themed'
+import { UserPhoto } from '@components/UserPhoto'
+import { Center, VStack } from '@gluestack-ui/themed'
+import { ScrollView } from 'react-native'
 
 export function Profile() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
+
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+        <Center mt="$6" px="$10">
+          <UserPhoto
+            source={{ uri: 'https://github.com/arthurrios.png' }}
+            size="xl"
+            alt="Imagem do usuário"
+          />
+        </Center>
+      </ScrollView>
     </VStack>
   )
 }
