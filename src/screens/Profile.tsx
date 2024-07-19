b0854e6e005a3ecfae932141eb2c38e29d4cb978
arthur <arthur.rios007@gmail.com>
2024-07-19 20:36:39 -0300

    Finalizando as interfaces: Botão alterar foto

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,7 +1,7 @@
 import { ScreenHeader } from '@components/ScreenHeader'
 import { UserPhoto } from '@components/UserPhoto'
-import { Center, VStack } from '@gluestack-ui/themed'
-import { ScrollView } from 'react-native'
+import { Center, Text, VStack } from '@gluestack-ui/themed'
+import { ScrollView, TouchableOpacity } from 'react-native'
 
 export function Profile() {
   return (
@@ -15,6 +15,18 @@ export function Profile() {
             size="xl"
             alt="Imagem do usuário"
           />
+
+          <TouchableOpacity>
+            <Text
+              color="$green500"
+              fontFamily="$heading"
+              fontSize="$md"
+              mt="$2"
+              mb="$8"
+            >
+              Alterar Foto
+            </Text>
+          </TouchableOpacity>
         </Center>
       </ScrollView>
     </VStack>
