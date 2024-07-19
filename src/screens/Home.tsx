251587f38d5454827193f8c6141f3fc27dc452d3
arthur <arthur.rios007@gmail.com>
2024-07-18 21:00:17 -0300

    Finalizando as interfaces: Criando componente ExerciseCard

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { ExerciseCard } from '@components/ExerciseCard'
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
 import { Heading, HStack, Text, VStack } from '@gluestack-ui/themed'
@@ -37,6 +38,8 @@ export function Home() {
             4
           </Text>
         </HStack>
+
+        <ExerciseCard />
       </VStack>
     </VStack>
   )
