fe9eaf5deec6e8b3b46f2b1bb7c3fe8d3e1e4ab9
arthur <arthur.rios007@gmail.com>
2024-07-22 19:23:33 -0300

    Galeria de Fotos: Buscando informções do arquivo

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -7,6 +7,7 @@ import { Center, Heading, Text, VStack } from '@gluestack-ui/themed'
 import * as ImagePicker from 'expo-image-picker'
 import { useState } from 'react'
 import { ScrollView, TouchableOpacity } from 'react-native'
+import * as FileSystem from 'expo-file-system'
 
 export function Profile() {
   const [userPhoto, setUserPhoto] = useState(
@@ -25,7 +26,15 @@ export function Profile() {
       return
     }
 
-    setUserPhoto(photoSelected.assets[0].uri)
+    const photoUri = photoSelected.assets[0].uri
+
+    if (photoUri) {
+      const photoInfo = (await FileSystem.getInfoAsync(photoUri)) as {
+        size: number
+      }
+
+      setUserPhoto(photoSelected.assets[0].uri)
+    }
   }
 
   return (
