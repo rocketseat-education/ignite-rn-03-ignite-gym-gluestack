758141e0c05a7ca92780f363f0efd4662094293d
arthur <arthur.rios007@gmail.com>
2024-07-22 22:39:57 -0300

    Formulários: Rules Patterns

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -92,6 +92,13 @@ export function SignUp() {
             <Controller
               control={control}
               name="email"
+              rules={{
+                required: 'Informe o e-mail',
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: 'E-mail inválido',
+                },
+              }}
               render={({ field: { onChange, value } }) => (
                 <Input
                   placeholder="E-mail"
@@ -102,6 +109,8 @@ export function SignUp() {
                 />
               )}
             />
+
+            <Text color="$white">{errors.email?.message}</Text>
 
             <Controller
               control={control}
